13f6bc65a42a305a3352fa2f04bf8d1ebd2a2c53
suheyb yusuf bashir <107487547+suheyb11@users.noreply.github.com>
2022-11-30 12:07:33 +0300

    Create 1-last_digit.py

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,8 @@
+import random
+number = random.randint(-10000, 10000)
+if number > 5:
+    print("{} and is greater than 5".format(number))
+elif number == 0:
+    print("{} and is 0".format(number))
+elif number < 6 && number != 0:
+    print("{} and is less than 6 and not 0".format(number))
