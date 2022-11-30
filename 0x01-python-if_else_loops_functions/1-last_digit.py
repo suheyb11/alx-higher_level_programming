ed4199c35e9a6b34e6d4c62d774379b90603ba09
suheyb yusuf bashir <107487547+suheyb11@users.noreply.github.com>
2022-11-30 12:10:24 +0300

    Update 1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,5 +5,5 @@ if number > 5:
     print("{} and is greater than 5".format(number))
 elif number == 0:
     print("{} and is 0".format(number))
-elif number < 6 && number != 0:
+elif (number < 6 )&& (number != 0):
     print("{} and is less than 6 and not 0".format(number))
